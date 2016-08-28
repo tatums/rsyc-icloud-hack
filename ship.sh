d1c9cc026a5df1b90720bd9bcf1771ca681779f1
Tatum Szymczak <tatum@ashlandstudios.com>
2016-08-28 09:55:50 -0500

    update ship script to use $HOME

--- ship.sh
+++ ship.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp rsync.code.to.icloud.folder.plist /Users/tatumszymczak/Library/LaunchAgents/rsync.code.to.icloud.folder.plist
+cp rsync.code.to.icloud.folder.plist $HOME/Library/LaunchAgents/rsync.code.to.icloud.folder.plist
 cp rsync-code-to-icloud-folder.sh /usr/local/bin/rsync-code-to-icloud-folder.sh
