9b74a97f0d53e0efd286f2725844eb628dbcc21d
Tatum Szymczak <tatum@ashlandstudios.com>
2016-08-28 13:08:35 -0500

    renmae template files

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,9 @@ SCRIPT_PATH="/usr/local/bin/${LABEL}.sh"
 PLIST="${LABEL}.plist"
 PLIST_PATH="${HOME}/Library/LaunchAgents/${PLIST}"
 
-sed "s|{{WATCH_PATH}}|${WATCH_PATH}|; s|{{SCRIPT_PATH}}|${SCRIPT_PATH}|; s|{{LABEL}}|${LABEL}|" plist-template > $PLIST_PATH
+sed "s|{{WATCH_PATH}}|${WATCH_PATH}|; s|{{SCRIPT_PATH}}|${SCRIPT_PATH}|; s|{{LABEL}}|${LABEL}|" plist.template > $PLIST_PATH
 ## take note of the extra slash / after WATCH_PATH.  IT's important
-sed "s|{{SRC}}|${WATCH_PATH}/|; s|{{DESTINATION_PATH}}|${DESTINATION_PATH}|;" shell-template > $SCRIPT_PATH
+sed "s|{{SRC}}|${WATCH_PATH}/|; s|{{DESTINATION_PATH}}|${DESTINATION_PATH}|;" shell.template > $SCRIPT_PATH
 
 if [ -f $PLIST_PATH ]; then
   launchctl unload $PLIST_PATH
